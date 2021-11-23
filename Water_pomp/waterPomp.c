4b398687899e1ebdde8607c13380f20d7977d675
E.S. Igbinosun <stefano.igbinosun@hva.nl>
2021-11-23 16:36:54 +0100

    make the pomp work

---
+++ Water_pomp/waterPomp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define POMP    7
+
+int main(void)
+{
+    printf("Water POMP is aan\n");
+    wiringPiSetup ();
+    pinMode (POMP, OUTPUT);
+
+    for (;;){
+        printf("Pomp on\n");
+        digitalWrite (POMP,1);
+        delay (3000);
+        prinf("Pomp off\n")
+        digitalWrite (POMP,0);
+        delay (3000);
+
+
+    }
+    return 0;
+}//
+// Created by stefano on 18/11/2021.
+//
+
